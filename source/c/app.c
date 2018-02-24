0d65474ac12b39938184c5828552c53bf08e65da
Casper 'fvlvte' Fałat <me@falat.me>
2018-02-24 23:53:30 +0100

    TCP server improvements.

--- source/c/app.c
+++ source/c/app.c
@@ -1,12 +1,20 @@
 #include <isdite/foundation/app.h>
 #include <isdite/foundation/log.h>
+#include <isdite/foundation/tcp_server.h>
 #include <stdio.h>
+#include <unistd.h>
 
 isResult AppMain(int argc, char ** argv)
 {
-  isdite_fn_syslog(1, "IRA Startup!");
+  isdite_fdn_fsyslog(IL_INFO, "IRA Startup!");
 
-  
+  isdite_fn_tcpServer_create("0.0.0.0", 8080, 100000);
+
+  while(1==1)
+  {
+    usleep(1000);
+  }
+
   return isSuccess;
 }
 
