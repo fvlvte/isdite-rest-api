6498cb7e1e7d96f98c092a916c0614a25c1d5330
Casper 'fvlvte' Fałat <me@falat.me>
2018-03-04 23:16:56 +0100

    Test REST server implementation.

--- source/c/app.c
+++ source/c/app.c
@@ -1,19 +1,41 @@
 #include <isdite/foundation/app.h>
 #include <isdite/foundation/log.h>
-#include <isdite/foundation/tcp_server.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <isdite/foundation/https_server.h>
+#include <string.h>
+
+void App_onGetHello
+(
+  is_tcpServer server,
+  is_tcpClient client,
+  is_string data,
+  int size,
+  void (*callback)(const void *, const void*, const void*, int, int),
+  is_httpsServer instance
+)
+{
+  const char * msg = "{\"message\":\"hello world\"}";
+  callback(server, client, msg, strlen(msg), 1);
+}
 
 isResult AppMain(int argc, char ** argv)
 {
   isdite_fdn_fsyslog(IL_INFO, "IRA Startup!");
 
-  isdite_fn_tcpServer_create("0.0.0.0", 8080, 100000);
+  is_httpsServer s =
+    isdite_net_httpsServer_create
+    (
+      "0.0.0.0",
+      443,
+      "cert.der",
+      "priv.der"
+    );
 
-  while(1==1)
-  {
-    usleep(1000);
-  }
+  is_httpsVhost vh = isdite_net_httpsServer_addVirtualHost(s, "127.0.0.1:8080");
+  is_httpsVhost vh2 = isdite_net_httpsServer_addVirtualHost(s, "api.isdite.com");
+  isdite_net_httpsServer_bindEndpoint(vh, "/hello", "GET", &App_onGetHello);
+  isdite_net_httpsServer_bindEndpoint(vh2, "/hello", "GET", &App_onGetHello);
+
+  isdite_net_httpsServer_join(s);
 
   return isSuccess;
 }
